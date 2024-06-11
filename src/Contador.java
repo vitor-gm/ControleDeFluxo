6c90930e52b220d404f75a789d9aafbfb071e31a
vitor-gm <gm.0509rbt@gmail.com>
2024-06-11 12:26:37 -0300

    Controle de Fluxo

---
+++ src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int primeiroNumero = terminal.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int segundoNumero = terminal.nextInt();
+
+        try {
+
+            contar(primeiroNumero, segundoNumero);
+
+        }catch(ParametrosInvalidosException e){
+
+            System.out.println("O segundo número deve ser maior que o primeiro.");
+
+        }
+
+    }
+
+    static void contar(int primeiroNumero, int segundoNumero) throws ParametrosInvalidosException {
+
+        if(segundoNumero < primeiroNumero){
+            throw new ParametrosInvalidosException();
+        }else{
+            int resultado = segundoNumero - primeiroNumero;
+            for(int i = 1; i <= resultado; i++){
+                System.out.println("Imprimindo número " + i);
+            }
+        }
+
+    }
+    
+}
